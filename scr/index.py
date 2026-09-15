10a9cdbef138fa09108a256f945e0ad501237592
Noah Golik <e12530040@student.tuwien.ac.at>
2026-09-16 01:58:11 +0200

    Inital Commit: csv downloader

---
+++ scr/index.py
@@ -0,0 +1,9 @@
+from wlDataProcessor import getcsvdata
+
+urls: list = [
+    "https://www.wienerlinien.at/ogd_realtime/doku/ogd/wienerlinien-ogd-haltepunkte.csv",  # stopPoints
+    "https://www.wienerlinien.at/ogd_realtime/doku/ogd/wienerlinien-ogd-haltestellen.csv",  # stopGroups
+    "https://www.wienerlinien.at/ogd_realtime/doku/ogd/wienerlinien-ogd-linien.csv",  # lines
+]
+
+getcsvdata(urls)
---
+++ scr/wlDataProcessor.py
@@ -0,0 +1,14 @@
+import os
+from pathlib import Path
+from urllib.parse import urlparse
+import urllib.request
+
+def getcsvdata(urls: list) -> None:
+    Path("../data").mkdir(parents=True, exist_ok=True)
+    for url in urls:
+        try:
+            p = Path("../data") / os.path.basename(urlparse(url).path)
+            urllib.request.urlretrieve(url, p)
+            print(p)
+        except Exception as e:
+            print(f"Error occurred while retrieving {url}: {e}")
